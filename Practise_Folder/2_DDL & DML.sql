70da1c2b6591fc604c7cfa9873893adedd64a5ba
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-06 05:32:55 +0100

    Update SQL projects

--- Practise_Folder/2_DDL & DML.sql
+++ Practise_Folder/2_DDL & DML.sql
@@ -137,3 +137,33 @@ FROM (SELECT *
         OR salary_hour_avg IS NOT NULL
 )
 LIMIT 10;
+
+--CTE--
+WITH valid_salaries AS (
+    SELECT *
+    FROM job_postings_fact
+    WHERE salary_year_avg IS NOT NULL
+        OR salary_hour_avg IS NOT NULL
+)
+SELECT *
+FROM valid_salaries
+LIMIT 10;
+
+--SCENARIOS
+--subquery in select
+-- show each job salary next to  the overall market median:
+SELECT
+    job_title_short,
+    salary_year_avg,
+    (
+        SELECT MEDIAN(salary_year_avg)
+        FROM job_postings_fact
+    )   AS market_median_salary
+FROM job_postings_fact
+WHERE salary_year_avg IS NOT NULL
+LIMIT 10;
+
+--stage only jobs that are remote before aggregating:
+
+
+-- KEEP ONLY TITLES WHOS MEDIAN SALARY IS AVOVE THE OVERALL MEDIAN:
