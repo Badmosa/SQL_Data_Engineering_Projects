8642c03c845f734cc8100f8c65bfef86dbdb2fac
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-07 19:01:35 +0100

    Update SQL projects

--- Practise_Folder/Priority_job_snap_initial.sql
+++ Practise_Folder/Priority_job_snap_initial.sql
@@ -1,23 +1,31 @@
-CREATE OR REPLACE TABLE main.priority_jobs_snapshot(
-    job_id              INTEGER PRIMARY KEY,
-    job_title_short     VARCHAR,
-    company_name        VARCHAR,
-    job_posted_date     TIMESTAMP,
-    salajry_year_avg    DOUBLE,
-    priority_lvl        INTEGER,
-    updated_at        TIMESTAMP
+-- SQLBook: Code
+SHOW TABLES FROM staging;
+
+ALTER TABLE main.priority_jobs_snapshot
+RENAME COLUMN salajry_year_avg TO salary_year_avg;
+
+DESCRIBE main.priority_jobs_snapshot;
+
+
+CREATE OR REPLACE TABLE main.priority_jobs_snapshot (
+    job_id           INTEGER PRIMARY KEY,
+    job_title_short  VARCHAR,
+    company_name     VARCHAR,
+    job_posted_date  TIMESTAMP,
+    salary_year_avg  DOUBLE,
+    priority_lvl     INTEGER,
+    updated_at       TIMESTAMP
 );
 
-INSERT INTO  main.priority_jobs_snapshot (
-   job_id,
-    job_title_short,    
+INSERT INTO main.priority_jobs_snapshot (
+    job_id,
+    job_title_short,
     company_name,
     job_posted_date,
     salary_year_avg,
     priority_lvl,
     updated_at
 )
-
 SELECT
     jpf.job_id,
     jpf.job_title_short,
@@ -26,20 +34,18 @@ SELECT
     jpf.salary_year_avg,
     r.priority_lvl,
     CURRENT_TIMESTAMP
-FROM
-    data_jobs.job_postings_fact AS jpf
+FROM data_jobs.job_postings_fact AS jpf
 LEFT JOIN data_jobs.company_dim AS cd
     ON jpf.company_id = cd.company_id
 INNER JOIN staging.priority_roles AS r
     ON jpf.job_title_short = r.role_name;
 
-    SELECT
-        job_title_short,
-        count(*) AS job_count,
-        MIN(priority_lvl) AS priority_lvl
-        MIN(updated_at) AS updated_at
-FROM priority_jobs_snapshot
+SELECT
+    job_title_short,
+    COUNT(*) AS job_count,
+    MIN(priority_lvl) AS priority_lvl,
+    MIN(updated_at) AS updated_at
+FROM main.priority_jobs_snapshot
 GROUP BY job_title_short
 ORDER BY job_count DESC;
 
-SHOW TABLES FROM staging;
