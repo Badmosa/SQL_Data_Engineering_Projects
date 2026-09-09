bcb9904e8a1879b2dee5011ef7bbc2bd1cc885a6
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-09 05:18:19 +0100

    Update SQL projects

--- Practise_Folder/Date_Function.sql
+++ Practise_Folder/Date_Function.sql
@@ -10,8 +10,11 @@ LIMIT 10;
 SELECT
     DATE_TRUNC('month', job_posted_date) AS job_posted_month,
     COUNT(job_id) AS job_count
-FROM job_postings_fact
-WHERE job_title_short = 'Data Engineer'
+FROM 
+    job_postings_fact
+WHERE 
+    job_title_short = 'Data Engineer' AND
+    EXTRACT(YEAR FROM job_posted_date) = 2024
 GROUP BY 
     DATE_TRUNC('month', job_posted_date)
 ORDER BY
@@ -30,3 +33,28 @@ FROM job_postings_fact
 ORDER BY RANDOM()
 LIMIT 10;
 
+-- AT TIMEZONE--
+SELECT
+    '2026-01-01 00:00:00+00'::TIMESTAMPTZ AT TIME ZONE 'EST';
+
+SELECT
+    job_title_short,
+    job_location,
+    job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'EST'
+FROM
+    job_postings_fact
+WHERE
+    job_location LIKE 'New York, NY';
+
+
+SELECT
+    EXTRACT(HOUR FROM job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'EST') AS job_posted_hour,
+    COUNT(job_id)
+FROM
+    job_postings_fact
+WHERE
+    job_location LIKE 'New York, NY'
+GROUP BY
+    EXTRACT(HOUR FROM job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'EST')
+ORDER BY
+    job_posted_hour;
