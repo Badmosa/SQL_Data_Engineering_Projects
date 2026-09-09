a63b83b42c4b7069f41123c7bb8776af07bc3872
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-09 04:38:09 +0100

    Update SQL projects

---
+++ Practise_Folder/Date_Function.sql
@@ -0,0 +1,32 @@
+SELECT 
+    job_posted_date,
+    job_posted_date::DATE AS date,
+    job_posted_date:: TIME AS time,
+    job_posted_date:: TIMESTAMPTZ AS timestampz
+FROM job_postings_fact
+LIMIT 10;
+
+--Extract--
+SELECT
+    DATE_TRUNC('month', job_posted_date) AS job_posted_month,
+    COUNT(job_id) AS job_count
+FROM job_postings_fact
+WHERE job_title_short = 'Data Engineer'
+GROUP BY 
+    DATE_TRUNC('month', job_posted_date)
+ORDER BY
+    job_posted_month;
+
+--Date_Trunc--
+SELECT
+    job_posted_date,
+    DATE_TRUNC('year', job_posted_date) AS truncated_year,
+    DATE_TRUNC('quarter', job_posted_date) AS truncated_quarter,
+    DATE_TRUNC('month', job_posted_date) AS truncated_month,
+    DATE_TRUNC('week', job_posted_date) AS truncated_week,
+    DATE_TRUNC('day', job_posted_date) AS truncated_day,
+    DATE_TRUNC('hour', job_posted_date) AS truncated_hour
+FROM job_postings_fact
+ORDER BY RANDOM()
+LIMIT 10;
+
