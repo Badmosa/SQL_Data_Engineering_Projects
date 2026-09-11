17580db0c080ca61661fbe231638d138d894b1fd
Badmos Adesola <badmosayomide02@gmail.com>
2026-09-11 05:49:31 +0100

    Update SQL projects

--- Practise_Folder/Nested_Data_Types.sql
+++ Practise_Folder/Nested_Data_Types.sql
@@ -98,3 +98,36 @@ SELECT
          type := json_extract_string(skill_json, '$.type')
     )
 FROM raw_skill;
+
+
+--Arrays--
+--Build a flat skiill table for co-workers to access job titles, salary info, and skills in one table--
+CREATE OR REPLACE TEMP TABLE job_skills_array AS
+SELECT
+    jpf.job_id,
+    jpf.job_title_short,
+    jpf.salary_year_avg,
+    ARRAY_AGG(sd.skills) AS skills_array
+FROM job_postings_fact AS jpf
+LEFT JOIN skills_job_dim AS sjd
+    ON jpf.job_id = sjd.job_id
+LEFT JOIN skills_dim AS sd
+    ON sd.skill_id = sjd.skill_id
+GROUP BY ALL;
+
+-- Analyze the median salary per skill--SELECT ['python', 'sql', 'r'] AS skills_array;
+
+WITH skills AS (
+    SELECT 'python' AS skill
+    UNION ALL
+    SELECT 'sql'
+    UNION ALL
+    SELECT 'r'
+), skills_array AS (
+SELECT ARRAY_AGG(skills ORDER BY skills) AS skills
+FROM skills
+)
+SELECT
+    skills
+FROM
+    skills_array;
